49ad181ca9d02fc1871edc86d94f9802b2626c06
Ilse <magi3654@gmail.com>
2025-02-18 11:05:42 -0800

    fixing the navigation url beetween index and dashboard

--- src/app.py
+++ src/app.py
@@ -1,14 +1,18 @@
- 
 from flask import Flask, render_template
 
-app = Flask (__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/header')
+def header():
+    return render_template('header.html')
+
 @app.route('/dashboard')
 def dashboard():
+    print('Accediendo a DASHBOARD')
     return render_template('dashboard.html')
 
 @app.route('/desgloses')
@@ -20,12 +24,12 @@ def papeletas():
     return render_template('papeletas.html')
 
 @app.route('/newclient')
-def clients():
+def newclient():
     return render_template('registroEmpresas.html')
 
 @app.route('/newuser')
-def user():
+def newuser():
     return render_template('registroUsuarios.html')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('header.html')
-        .then(response => response.text())
+    fetch('/header')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.text();
+        })
         .then(data => {
             document.getElementById('header-container').innerHTML = data;
         })
